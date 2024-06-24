35700cb22956cab096093fc5f84e7ff6e2cc0fc7
Ibrahim-Elassal <ibrahimelassal662@gmail.com>
2024-06-24 14:50:10 +0200

    update app.js file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
       <h1>App Component</h1>
       <h2>Lorem, ipsum dolor.</h2>
       <h2>Lorem, ipsum dolor.</h2>
+
+      <p>updat Lorem ipsum dolor sit amet.</p>
     </section>
   );
 }
